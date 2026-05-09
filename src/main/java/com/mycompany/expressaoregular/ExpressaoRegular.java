b86ad70c8bfeda870208814278e7a3c79d36e2bc
PedroPivaDoNascimento <pedropiva9@gmail.com>
2026-05-08 23:47:35 -0300

    Implementação dos exercícios restantes

--- src/main/java/com/mycompany/expressaoregular/ExpressaoRegular.java
+++ src/main/java/com/mycompany/expressaoregular/ExpressaoRegular.java
@@ -4,13 +4,70 @@
 
 package com.mycompany.expressaoregular;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  *
  * @author piva
  */
 public class ExpressaoRegular {
 
+    public static String testResult(String regex, String word) {
+        if (RegexHandler.check(regex, word)) {
+            return "Sucesso";
+        }
+        
+        return "Falha";
+    }
+    public static void testExercice(List<String> ex, String language){
+    
+        for (String word : ex) {
+            System.out.println(testResult(language, word));
+        }
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Exercício 02: Contém aa ou bb
+        List<String> ex02 = new ArrayList<>(Arrays.asList(
+            "aa", "bba", "aabba", "baab", "bbaa", // Sucessos
+            "a", "b", "abab", "aba", "baba"       // Falhas
+        ));
+        System.out.println("Exercício 02:");
+        testExercice(ex02, RegexHandler.getLANGUAGE01());
+
+        // Exercício 03: Quantidade par de a e de b
+        List<String> ex03 = new ArrayList<>(Arrays.asList(
+            "aabb", "abab", "aaaa", "bb", "",    // Sucessos
+            "a", "b", "aaab", "abb", "aba"       // Falhas
+        ));
+        System.out.println("Exercício 03:");
+        testExercice(ex03, RegexHandler.getLANGUAGE02());
+
+        // Exercício 04: Começa e termina com 'a'
+        List<String> ex04 = new ArrayList<>(Arrays.asList(
+            "aa", "aba", "aaaaa", "abba", "aabaa", // Sucessos
+            "ab", "ba", "b", "abc", "bb"           // Falhas
+        ));
+        System.out.println("Exercício 04:");
+        testExercice(ex04, RegexHandler.getLANGUAGE03());
+
+        // Exercício 05: E-mail
+        List<String> ex05 = new ArrayList<>(Arrays.asList(
+            "teste@gmail.com", "user.123@uol.com.br", "a@b.co", "email@empresa.org", "dev_jr@web.net", // Sucessos
+            "email@", "@gmail.com", "joao@com", "espaco @gmail.com", "a.b.c"                          // Falhas
+        ));
+        System.out.println("Exercício 05:");
+        testExercice(ex05, RegexHandler.getEMAIL());
+
+        // Exercício 06: Reais negativos com expoente
+        List<String> ex06 = new ArrayList<>(Arrays.asList(
+            "-1.5e10", "-0.5E-2", "-123.45e+5", "-10.0e2", "-3.14e0", // Sucessos
+            "21123--312", "-1.5-", "-e10", "-1.5e", "-123-.5e5.5"             // Falhas
+        ));
+        System.out.println("Exercício 06:");
+        testExercice(ex06, RegexHandler.getEXPONENTIAL_NUMBERS());
+
     }
 }
--- src/main/java/com/mycompany/expressaoregular/RegexHandler.java
+++ src/main/java/com/mycompany/expressaoregular/RegexHandler.java
@@ -9,18 +9,36 @@ package com.mycompany.expressaoregular;
  * @author piva
  */
 public class RegexHandler {
-    private final String LANGUAGE01 = "(a|b)*((aa)|(bb))(a|b)*";
-    private final String LANGUAGE02 = "(aa|bb|(ab|ba)(aa|bb)*(ab|ba))*";
-    private final String LANGUAGE03 = "a(a|b)*a";
+    private final static String LANGUAGE01 = "(a|b)*((aa)|(bb))(a|b)*";
+    private final static String LANGUAGE02 = "((aa|bb)|(ab|ba)(aa|bb)*(ab|ba))*";
+    private final static String LANGUAGE03 = "a(b|a)*a";
     
-    private final String LETTER = "([A-Za-z])";
-    private final String NUMBER = "([0-9])";
-    private final String WORD = "("+LETTER+"|"+NUMBER+")";
-    private final String BEFORE_DOMAIN = "(("+WORD+"|(\\-|\\_))+(\\.)?("+WORD+"|(\\-|\\_))+)+";
-    private final String AFTER_DOMAIN = "@("+WORD+")+(\\_)?("+WORD+"|\\.|)+("+WORD+")+";
-    private final String EMAIL = "(("+BEFORE_DOMAIN+"){1,64}"+AFTER_DOMAIN+"){1,254}";
+    private final static String LETTER = "([A-Za-z])";
+    private final static String NUMBER = "([0-9])";
+    private final static String WORD = "("+LETTER+"|"+NUMBER+")";
+    private final static String BEFORE_DOMAIN = "(("+WORD+"|(\\-|\\_))+(\\.)?("+WORD+"|(\\-|\\_))*)+";
+    private final static String AFTER_DOMAIN = "("+WORD+")+(\\_)?("+WORD+"|\\.)*("+WORD+")*(\\.)("+WORD+")+";
+    private final static String EMAIL = "(("+BEFORE_DOMAIN+"){1,64}@"+AFTER_DOMAIN+"){1,254}";
     
-    
+    private final static String REAL_NUMBERS = "("+NUMBER+")+(.("+NUMBER+")+)?";
+    private final static String NEGATIVE_REAL_NUMBERS = "(-)?("+REAL_NUMBERS+")";
+    private final static String EXPONENTIAL_NUMBERS = "("+NEGATIVE_REAL_NUMBERS+")((E|e)(\\+|\\-)?("+REAL_NUMBERS+"))?"; 
+
+    public static String getLANGUAGE01() {
+        return LANGUAGE01;
+    }
+    public static String getLANGUAGE02() {
+        return LANGUAGE02;
+    }
+    public static String getLANGUAGE03() {
+        return LANGUAGE03;
+    }
+    public static String getEMAIL() {
+        return EMAIL;
+    }
+    public static String getEXPONENTIAL_NUMBERS() {
+        return EXPONENTIAL_NUMBERS;
+    }
     
     public static boolean check(String regex, String word) {
         return word.matches(regex);
